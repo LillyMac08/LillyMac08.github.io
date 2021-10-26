e9ada40657229afbba385c64cc3844f7862b0e9e
LillyMac08 <70480952+LillyMac08@users.noreply.github.com>
2021-10-26 12:28:43 -0600

    changing what changes
    only one element (test) is changed colors

--- gitJS.js
+++ gitJS.js
@@ -3,6 +3,10 @@ function testFunc() {
 }
 
 function button2() {
+    /*
     var p = document.getElementsByTagName("p");
     p.style.color = "red";
+    */
+   var elem = document.getElementById("test");
+   elem.style.color = "#B00B69";
 }
